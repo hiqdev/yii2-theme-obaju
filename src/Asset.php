b08a745a89c73ecfa6f0e98290cba92497b9d751
tofid <andreyklochok@gmail.com>
2016-12-26 19:29:49 +0200

    Added main theme files and assets

---
+++ src/Asset.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace hiqdev\themes\obaju;
+
+use yii\web\AssetBundle;
+
+/**
+ * Theme main asset bundle.
+ */
+class Asset extends AssetBundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $sourcePath = '@hiqdev/themes/obaju/assets';
+
+    /**
+     * {@inheritdoc}
+     */
+    public $css = [
+    ];
+
+    public $js = [
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public $depends = [
+    ];
+}
---
+++ src/Theme.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hiqdev\themes\obaju;
+
+class Theme extends \hiqdev\thememanager\Theme
+{
+    public $label = 'Obaju';
+
+    public $assets = [
+        Asset::class,
+    ];
+}
