9e5419ba9ae8425ac0a91f68d11de067bb220d15
tofid <andreyklochok@gmail.com>
2016-12-30 18:51:10 +0200

    minor

--- src/widgets/views/LogoLink.php
+++ src/widgets/views/LogoLink.php
@@ -2,11 +2,11 @@
 
 use yii\helpers\Html;
 
-print Html::beginTag('a', ['href' => $url]);
-print Html::img($image, $options);
-print Html::beginTag('img', array_merge(['src' => $smallImage], $smallImageOptions));
-print Html::tag('span', $name, ['class' => 'sr-only']);
-print Html::endTag('img');
-print Html::endTag('a');
+echo Html::beginTag('a', array_merge(['href' => $url], $options));
+    echo Html::img($image, $imageOptions);
+    printf('<img src="%s" %s>', $smallImage, Html::renderTagAttributes($smallImageOptions));
+        echo Html::tag('span', $name, ['class' => 'sr-only']);
+    echo '</img>';
+echo Html::endTag('a');
 
 ?>
