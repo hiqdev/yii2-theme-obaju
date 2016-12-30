32423ee82b21440a9dd19c4125a0729f2d416aeb
tofid <andreyklochok@gmail.com>
2016-12-30 19:08:14 +0200

    Added footer dummy content

---
+++ src/views/layouts/_after_footer.php
@@ -0,0 +1,17 @@
+<!-- *** COPYRIGHT ***
+_________________________________________________________ -->
+<div id="copyright">
+    <div class="container">
+        <div class="col-md-6">
+            <p class="pull-left">© 2015 Your name goes here.</p>
+
+        </div>
+        <div class="col-md-6">
+            <p class="pull-right">Template by <a
+                    href="https://bootstrapious.com/e-commerce-templates">Bootstrapious.com</a>
+                <!-- Not removing these links is part of the license conditions of the template. Thanks for understanding :) If you want to use the template without the attribution links, you can do so after supporting further themes development at https://bootstrapious.com/donate  -->
+            </p>
+        </div>
+    </div>
+</div>
+<!-- *** COPYRIGHT END *** --
