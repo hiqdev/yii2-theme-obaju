706ee25650a8a4cdae64a7bc2871c3151e554f12
tofid <andreyklochok@gmail.com>
2017-01-06 18:12:24 +0200

    Fixed assets

--- src/Asset.php
+++ src/Asset.php
@@ -20,6 +20,8 @@ class Asset extends AssetBundle
      */
     public $css = [
         '//fonts.googleapis.com/css?family=Roboto:400,500,700,300,100',
+        'css/font-awesome.css',
+        'css/animate.min.css',
         'css/style.default.css',
         'css/custom.css',
     ];
@@ -28,10 +30,12 @@ class Asset extends AssetBundle
      * {@inheritdoc}
      */
     public $js = [
-//        'js/respond.min.js',
-//        'js/modernizr.js',
-//        'js/bootstrap-hover-dropdown.js',
-//        'js/front.js',
+        'js/respond.min.js',
+        'js/jquery.cookie.js',
+        'js/waypoints.min.js',
+        'js/modernizr.js',
+        'js/bootstrap-hover-dropdown.js',
+        'js/front.js',
     ];
 
     /**
