0054f1cc179912d183c6b797087b8b1b12e24b68
tofid <andreyklochok@gmail.com>
2017-01-19 19:17:46 +0200

    Added meta data

--- src/Theme.php
+++ src/Theme.php
@@ -6,6 +6,10 @@ class Theme extends \hiqdev\thememanager\Theme
 {
     public $label = 'Obaju';
 
+    public $description = 'Description';
+
+    public $screenshot = 'http://obaju.ondrejsvestka.cz/img/main-slider1.jpg';
+
     public $assets = [
         Asset::class,
     ];
