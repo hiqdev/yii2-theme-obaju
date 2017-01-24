7f1e998fadc064c63ecf4cca9e805a80f48aed92
tofid <andreyklochok@gmail.com>
2017-01-24 19:26:38 +0200

    Applied SocialLinks widget

--- src/views/layouts/_footer.php
+++ src/views/layouts/_footer.php
@@ -1,3 +1,7 @@
+<?php
+use hiqdev\thememanager\widgets\SocialLinks;
+
+?>
 <!-- *** FOOTER *** -->
 <div id="footer" data-animate="fadeInUp">
     <div class="container">
@@ -108,14 +112,14 @@
                 <h4>Stay in touch</h4>
 
                 <p class="social">
-                    <a href="#" class="facebook external" data-animate-hover="shake"><i class="fa fa-facebook"></i></a>
-                    <a href="#" class="twitter external" data-animate-hover="shake"><i class="fa fa-twitter"></i></a>
-                    <a href="#" class="instagram external" data-animate-hover="shake"><i
-                            class="fa fa-instagram"></i></a>
-                    <a href="#" class="gplus external" data-animate-hover="shake"><i class="fa fa-google-plus"></i></a>
-                    <a href="#" class="email external" data-animate-hover="shake"><i class="fa fa-envelope"></i></a>
+                    <?= SocialLinks::widget([
+                        'tag' => 'span',
+                        'linkOptions' => [
+                            'class' => 'external',
+                            'data-animate-hover' => 'shake',
+                        ],
+                    ]) ?>
                 </p>
-
 
             </div>
 
