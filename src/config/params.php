00bb8f6b7c931e4ebb2bca56766157ad3372bdb9
tofid <andreyklochok@gmail.com>
2017-01-27 17:55:48 +0200

    Addedd params file with thumbnail options

---
+++ src/config/params.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'thumb.catalog' => [
+        'thumbnail' => [
+            'width' => 260,
+            'height' => 180,
+        ],
+        'placeholder' => [
+            'width' => 260,
+            'height' => 180,
+        ]
+    ],
+    'thumb.carousel' => [
+
+    ],
+];
