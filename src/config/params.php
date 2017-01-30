b7b368e08927aa6b5d35fb38b674c4a4dc499c86
tofid <andreyklochok@gmail.com>
2017-01-30 12:00:28 +0200

    minor

--- src/config/params.php
+++ src/config/params.php
@@ -3,14 +3,27 @@ return [
     'thumb.catalog' => [
         'thumbnail' => [
             'width' => 260,
-            'height' => 180,
+            'height' => 340,
         ],
         'placeholder' => [
             'width' => 260,
-            'height' => 180,
-        ]
+            'height' => 340,
+        ],
     ],
     'thumb.carousel' => [
-
+        'thumbnail' => [
+            'width' => 1110,
+            'height' => 520,
+        ],
+        'placeholder' => [
+            'width' => 1110,
+            'height' => 520,
+        ],
+    ],
+    'thumb.detail' => [
+        'placeholder' => [
+            'width' => 600,
+            'height' => 800,
+        ],
     ],
 ];
